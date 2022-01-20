a2f185481eb8f0a1aa2dff89060094971c503edd
niamb2023 <niamb2023@headroyce.org>
2022-01-20 09:31:48 -0800

    fixing the BST tree

--- BST.js
+++ BST.js
@@ -1,11 +1,8 @@
-class BST {
+export default class BST {
     #root;
     #comparator;
     static BSTNode = class {
-        #data;
-        #left;
-        #right;
-//test
+    
         constructor(data){
             //data element
             this.#data = data;
@@ -13,7 +10,12 @@ class BST {
             this.#left = null;
             this.#right = null;
         }
-    }
+        getData(){
+            return this.data;
+        }
+        
+    };
+    #root = null;
 
 
    // comparator must be a function(a,b)
@@ -25,7 +27,7 @@ class BST {
       throw 'comparator not defined!';
    }
   constructor(comparator) {
-      this.#root = null;
+      this.root = null;
       if(#comparator !== null){
          this.#comparator = comparator;
      
@@ -38,156 +40,16 @@ class BST {
    * Adding a node to the bst
    **/
   add(data) {
+      const Node = BST.BSTNode(data);
+     
+     
       
-     
-     
-      if(data == null || data == ""){
-          return -1
-      }
-      let n = new BST.BSTNode(data)
-      let current = this.#root
-      if (current == null) { //BST is empty
-          this.#root = n //points head to n
-          
-      }
-      else{
-           let prev = null
-               
-           while(current != null){
-                   
-                   if( this.#comparator(data, current.data) <= 0 ){
-                       prev = current
-                       current = current.left;
-                        
-                   }
-                   else{
-                       prev = current
-                       current = current.right
-                     
-                   }
-           }
-         
-         
-          
-           if (isNaN(parseFloat(data)) && isNaN(parseFloat(this.#root.data)) || !isNaN(parseFloat(data)) && !isNaN(parseFloat(this.#root.data))){
-               
-               if (this.#comparator(data, prev.data) <= 0 ){       
-                   prev.left = n
-                   return 0;
-               }
-               else{
-                   prev.right = n;
-                   return 0;
-               }
-           }
-           else{
-               return 1;
-           }
-         
-      }
   }
    /**
    * Removing a node
    **/
   remove( data) {
-      if (data == null){
-          return
-      }
-      let curr = this.#root
-      let prev = curr
-      let data_num = data
-
-     
-      while(curr.data != data_num){
-          
-          if(curr.left == null && curr.right == null && curr.data !=  data_num){
-              return false
-          }
-          if (this.#comparator(data_num, curr.data) <= 0){
-              prev = curr
-              curr = curr.left
-          }
-          else{
-              prev = curr
-              curr = curr.right
-          }
-
-          
-          if(curr == null){
-              return false
-          }
-      }
-      let children_num = 0
-      if(curr.left != null){
-          children_num++
-      }
-      if(curr.right != null){
-          children_num++
-
-      }
-     
-      if(children_num == 1){
-     
-          if(curr == this.#root){
-              if(curr.left != null){
-                  this.#root = curr.left
-              }else{
-                  this.#root = curr.right
-              }
-          }
-           
-          if(this.#comparator(data_num, prev.data) > 0){
-              if(curr.right != null){
-                  prev.right = curr.right
-              }else{
-                  prev.right = curr.left
-              }
-          } else{
-              if(curr.left != null){
-                  prev.left = curr.left
-              }else{
-                  prev.left = curr.right
-              }
-          }
-          return data_num
-      }
-      //Deleting a leaf node
-      if(children_num == 0){
-          if(curr == this.#root){
-              this.#root = null
-          }
-          //Have to use parsefloat because of some weird bug happening here when attempting to remove 10 from the tree
-          if(this.#comparator(curr.data, prev.data) > 0){
-              prev.right = null
-          }else{
-              prev.left = null
-          }
-          return data
-      }
-     
-     
-      let tempNode = curr;
-      prev = curr
-     
-      //going to the left once and then to the right to find what node to replace it
-      if (curr.left != null){
-          curr = curr.left;
-      }
-      while (curr.right != null){
-          prev = curr
-          curr = curr.right;
-      }
-
-      //Moving that leaf node data up to where the data resided previously
-      tempNode.data = curr.data;
-      //deleting the leaf node
-      if(prev == tempNode){
-          prev.left = null
-      } else{
-          prev.right = null
-      }
-     
-      return tempNode.data
+      
   }
 
   /*
@@ -195,20 +57,17 @@ class BST {
   */
   
   inOrder() {
+    const rtnList = [];
+    const inOrderHelper = (node) => {
+        if (node?.left) inOrderHelper(node.left);
+        if (node) rtnList.push(node.data);
+        if (node?.right) inOrderHelper(node.right);
+    };
+    inOrderHelper(this.root);
+    return rtnList;
       
-      let inOrderHelper = function (n, curr){
-          if(n == null){
-              return;
-          }
-          inOrderHelper(n.left, curr);
-          curr.push(n.data);
-          inOrderHelper(n.right, curr);
-      }
-      let rtn = [ ];
-      inOrderHelper(this.#root, rtn);
-      return rtn;
+      
   }
  
-  #removeNode( node ) { }
-#findNode( data ) {}
+  
 }
